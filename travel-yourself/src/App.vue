c9666ee4fbe4960ac8928cc680455327b3daeca4
Derek Barbosa <dbarbosa@bu.edu>
2022-04-19 10:21:21 -0400

    bones

---
+++ travel-yourself/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/TTY.png">
+  <HelloWorld msg="Enter Your Destination"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//import SearchBar from './components/SearchBar.vue'
+//import BudgetBar from './components/BudgetBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
